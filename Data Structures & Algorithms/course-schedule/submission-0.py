f7be91e56c1f6d74150f9448daa36f8c865bd625
Novozhilov Ivan <94193311+novozhilov-ivan@users.noreply.github.com>
2026-09-15 18:25:42 +0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        schedule = {}
+
+        for prerequisite in prerequisites:
+            course_1, course_2 = prerequisite
+            if course_1 not in schedule:
+                schedule[course_1] = []
+            if course_2 not in schedule:
+                schedule[course_2] = []
+            schedule[course_1].append(course_2)
+        
+        finished = set()
+        q = deque(list(schedule.keys()))
+
+        while q and numCourses >= 0:
+            cour = q.popleft()
+
+            next_courses = schedule[cour]
+            if next_courses == [] and numCourses >= 0:
+                return True
+            
+            for c in next_courses:
+                q.append(c)
+
+            numCourses -= 1
+        
+        return False
+
+
+
